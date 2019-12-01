27a18ed0f96feb886362b4b13e87eb507c4321af
Andre Devecchi <andre.devecchi@gmail.com>
2019-12-01 00:01:22 -0300

    First commit

---
+++ api/src/dependencies.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+$container = $app->getContainer();
+
+// PDO database library
+$container['db'] = function ($c) {
+	$settings = $c->get('settings')['db'];
+	$pdo = new \PDO('mysql:host='.$settings['host'].';dbname='.$settings['dbname'].';charset=utf8', $settings['user'], $settings['pass']);
+	$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+	$pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+	return $pdo;
+};
---
+++ api/src/settings.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+return [
+	'settings' => [
+		'displayErrorDetails' => true,
+
+		// Database connection settings 
+		'db' => [
+			'host' => 'localhost',
+			'dbname' => 'teste_red',
+			'user' => 'root',
+			'pass' => 'devecchi'
+		]
+	]
+];
